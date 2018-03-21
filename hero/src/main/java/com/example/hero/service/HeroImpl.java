d7f4267ab08c0a6be5edaa8e4f26c0b2128b82ba
leprashimabuku <shimabuku@lepra.jp>
2018-03-21 15:59:41 +0900

    英雄を操作する実装クラスを作成

---
+++ hero/src/main/java/com/example/hero/service/HeroImpl.java
@@ -0,0 +1,67 @@
+/**
+ *
+ */
+package com.example.hero.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lepra
+ *
+ */
+@Service
+public class HeroImpl implements Hero {
+
+  @Autowired
+  private com.example.hero.mapper.Hero heroMapper;
+
+  /* (非 Javadoc)
+   * @see com.example.hero.service.Hero#getHeroList()
+   */
+  @Override
+  public List<com.example.hero.model.entity.Hero> getHeroList() {
+    return heroMapper.selectHeroList();
+  }
+
+  /* (非 Javadoc)
+   * @see com.example.hero.service.Hero#getHero(com.example.hero.model.entity.Hero)
+   */
+  @Override
+  public List<com.example.hero.model.entity.Hero> getHero(com.example.hero.model.entity.Hero hero) {
+    if(Objects.nonNull(hero.getId())) {
+      return Arrays.asList(heroMapper.selectHeroById(hero.getId()));
+    } else {
+      return heroMapper.selectHeroByName(hero.getName());
+    }
+  }
+
+  /* (非 Javadoc)
+   * @see com.example.hero.service.Hero#addHero(com.example.hero.model.entity.Hero)
+   */
+  @Override
+  public Boolean addHero(com.example.hero.model.entity.Hero hero) {
+    return heroMapper.insertHero(hero);
+  }
+
+  /* (非 Javadoc)
+   * @see com.example.hero.service.Hero#removeHeroName(java.lang.Integer)
+   */
+  @Override
+  public Boolean removeHeroName(Integer id) {
+    return heroMapper.deleteHero(id);
+  }
+
+  /* (非 Javadoc)
+   * @see com.example.hero.service.Hero#setHeroName(com.example.hero.model.entity.Hero)
+   */
+  @Override
+  public Boolean setHeroName(com.example.hero.model.entity.Hero hero) {
+    return heroMapper.deleteHero(hero);
+  }
+
+}
